e800417474cfad0194f6f748b1fabe82f092d1e3
sudinojkt <sudinosg@protonmail.com>
2022-10-23 08:07:40 +0000

    edit search

--- data/data.js
+++ data/data.js
@@ -1,6 +1,5 @@
 // all things related to retriving data with axios
 // goes here
-
 const API_BASE_URL = "https://api.foursquare.com/v3";
 const API_KEY = "fsq3QDBN0pfxk64qwMqNNvXhhDwbZOfIZqUEa4L/YjiNATQ="
 
@@ -11,23 +10,29 @@ const headers = {
     Authorization: API_KEY
 }
 
-async function searchPlace () {
-    let ll = "1.3521, 103.8198"; //for leaflet latlng is an array, for four square is a string
-    let response = await axios.get(API_BASE_URL + "/places/search", {
-        headers: {
-            ...headers
-        },
+async function searchPlace(ll, search, radius, category = "") {
+    let url = API_BASE_URL + "/places/search";
+    let response = await axios.get(url, {
+        headers: { ...headers },
         param: {
             ll: 'll',
-            query: 'query',
-            radius: 100000,  
-            limit: 50, 
+            query: 'searchPlace',
+            radius: radius,
+            category: category,
+            limit: 50,
+            v: 20210903,
         }
     });
-    console.log(response.data);
-    return response.dat;
+    console.log(response.data)
+    return response.data;
+}  
+
+//searchPlace("1.3521, 103.8198");
+
+async function main() {
+    searchPlace("1.527549, 103.75476", "fish soup", 5000)
 }
-searchPlace();
+main();
 
 
 //  the code below is working 
