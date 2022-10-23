7f37ff9adcb4d55dd203d7dead6a79a48ff2c7ac
sudinojkt <sudinosg@protonmail.com>
2022-10-23 09:51:36 +0000

    change line 12 lat lng

--- data/data.js
+++ data/data.js
@@ -1,7 +1,6 @@
 // all things related to retriving data with axios goes here
 const API_BASE_URL = "https://api.foursquare.com/v3";
 const API_KEY = "fsq3QDBN0pfxk64qwMqNNvXhhDwbZOfIZqUEa4L/YjiNATQ=";
-//const Singapore = "1.3521, 103.8198";
 
 // tells the Foursquare API server that the format of data we are sending is JSON
 const headers = {
@@ -10,23 +9,23 @@ const headers = {
     Authorization: API_KEY,
 }
 
-//async function searchPlace(ll, search, radius, category="")
-async function searchPlace(lat,lng, searchPlace) {
-    let url = API_BASE_URL + "/places/search";
-    let ll = lat + "," + lng; 
-    let response = await axios.get(url, {
-        headers: { ...headers },
-        params: {
-            ll: ll,
-            query: searchPlace,
-            radius: 100000,
-            category: 13000,
-            limit: 20,
-            v: 20210903,
-        }
-    });
-    console.log(response.data)
-    return response.data;
+async function searchPlace(ll, searchPlace, radius, category = "")
+//async function searchPlace(lat, lng, searchPlace) {
+let url = API_BASE_URL + "/places/search";
+let ll = lat + "," + lng;
+let response = await axios.get(url, {
+    headers: { ...headers },
+    params: {
+        ll: ll,
+        query: searchPlace,
+        radius: 100000,
+        category: 13000,
+        limit: 30,
+        v: 20210903,
+    }
+});
+console.log(response.data)
+return response.data;
 }
 
 async function main() {
