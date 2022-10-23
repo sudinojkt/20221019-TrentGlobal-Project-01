c14257b3333d92c8dd870ec510b43d0e6436c6d4
sudinojkt <sudinosg@protonmail.com>
2022-10-23 05:12:53 +0000

    try fsq js search

--- data/data.js
+++ data/data.js
@@ -11,18 +11,42 @@ const headers = {
     Authorization: API_KEY
 }
 
-async function main () {
-    let ll = "1.3521, 103.8198"; //for leaflet latlng is an array, for four square is a string
-    let response = await axios.get(API_BASE_URL + "search", {
-        "headers": headers,
-        "param": {
-            'll': ll,
-            'query': "chicken rice"
-            //'v': 20210903,         
-        }
-    });
-    console.log(response.data);
+// async function main () {
+//     let ll = "1.3521, 103.8198"; //for leaflet latlng is an array, for four square is a string
+//     let response = await axios.get(API_BASE_URL + "search", {
+//         "headers": headers,
+//         "param": {
+//             'll': ll,
+//             'query': "chicken rice"
+//             //'v': 20210903,         
+//         }
+//     });
+//     console.log(response.data);
+// }
+
+// main();
+
+async function placeSearch() {
+    try {
+        const searchParams = new URLSearchParams({
+          query: 'chicken rice',
+          ll: '1.3521, 103.8198',
+          open_now: 'true',
+          sort: 'DISTANCE'
+        });
+        const results = await fetch(
+          `https://api.foursquare.com/v3/places/search?${searchParams}`,
+          {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              Authorization: 'fsq3QDBN0pfxk64qwMqNNvXhhDwbZOfIZqUEa4L/YjiNATQ=',
+            }
+          }
+        );
+        const data = await results.json();
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
 }
-
-main();
-
