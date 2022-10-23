9ca9c05ae9a6a0ace6f81f0315554c3b3d90bf3b
sudinojkt <sudinosg@protonmail.com>
2022-10-23 06:22:20 +0000

    modify codes

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 // all things related to retriving data with axios
 // goes here
 
-const API_BASE_URL = "https://api.foursquare.com/v3/places/";
+const API_BASE_URL = "https://api.foursquare.com/v3";
 const API_KEY = "fsq3QDBN0pfxk64qwMqNNvXhhDwbZOfIZqUEa4L/YjiNATQ="
 
 // tells the Foursquare API server that the format of data we are sending is JSON
@@ -11,17 +11,41 @@ const headers = {
     Authorization: API_KEY
 }
 
-// async function main () {
-//     let ll = "1.3521, 103.8198"; //for leaflet latlng is an array, for four square is a string
-//     let response = await axios.get(API_BASE_URL + "search", {
-//         "headers": headers,
-//         "param": {
-//             'll': ll,
-//             'query': "chicken rice"
-//             //'v': 20210903,         
-//         }
-//     });
-//     console.log(response.data);
-// }
+async function searchPlace () {
+    let ll = "1.3521, 103.8198"; //for leaflet latlng is an array, for four square is a string
+    let response = await axios.get(API_BASE_URL + "/places/search", {
+        headers: {
+            ...headers
+        },
+        param: {
+            ll: 'll',
+            query: 'query',
+            radius: 100000,  
+            limit: 50, 
+        }
+    });
+    console.log(response.data);
+    return response.dat;
+}
+searchPlace();
 
-// main();
+
+//  the code below is working 
+// async function search(lat, lng, query) {
+//     // create the coordinate
+//     let ll = lat + "," + lng;
+//     let response = await axios.get(API_BASE_URL + "/places/search",{
+//         headers: {
+//             ...headers
+//         },
+//         params: {
+//             ll: ll,
+//             v: '20210903',  // YYYYMMDD format
+//             query: query
+//         }
+//     })
+//     console.log(response.data)
+//     return response.data;
+// }
+// search(1.3521,103.8198, "chicken rice")
+
