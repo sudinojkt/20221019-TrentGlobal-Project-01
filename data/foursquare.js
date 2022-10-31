864b0e67c3a9d499e2ebb534b6766aec7923b56f
unknown <sudinosg@protonmail.com>
2022-10-31 21:58:37 +0800

    rename data file, separate initMap file and remove unnecessary line

--- data/foursquare.js
+++ data/foursquare.js
@@ -1,5 +1,5 @@
 // All base URLs of API 
-const API_BASE_PLACES_URL = "https://api.foursquare.com/v3/places/"; 
+const API_BASE_PLACES_URL = "https://api.foursquare.com/v3/places/";
 
 // All API Keys
 const API_KEY = "fsq3QDBN0pfxk64qwMqNNvXhhDwbZOfIZqUEa4L/YjiNATQ=";
@@ -12,21 +12,15 @@ const headers = {
 }
 
 //Get place data function  
-async function search(latLng, search, categories="") {
+async function search(latLng, search, categories = "") {
     if (categories == '0') {
         categories = "13052, 13145, 13338";
-    }  
+    }
+
     let url = API_BASE_PLACES_URL + "search";
     let response = await axios.get(url, {
         "headers": headers,
-        "params": {
-            "ll": latLng,
-            "query": search,
-            "radius": 12500, 
-            "categories": categories,
-            "limit": 50,
-            "v": 20210903,
-        }
+        "params": { "ll": latLng, "query": search, "radius": 12500, "categories": categories, "limit": 50, }
     });
     return response.data;
 }
@@ -34,10 +28,6 @@ async function search(latLng, search, categories="") {
 //Get photos data 
 async function getPhoto(fsq_id) {
     let url = API_BASE_PLACES_URL + fsq_id + "/photos";
-    let response = await axios.get(url, {
-        'headers': headers,
-        "categorie": fsq_id,
-    });
-    //console.log(response.data);
+    let response = await axios.get(url, { "categorie": fsq_id, });
     return response.data;
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,25 @@
-window.addEventListener("DOMContentLoaded", async function(){
-    function init(){
+window.addEventListener("DOMContentLoaded", async function () {
+    function init() {
         let map = initMap();
-        let searchResultLayer = L.markerClusterGroup();
 
+        let searchResultLayer = L.markerClusterGroup();
         searchResultLayer.addTo(map);
-        document.querySelector("#btnToggleSearch").addEventListener("click", async function(){
+
+        document.querySelector("#btnToggleSearch").addEventListener("click", async function () {
             let searchContainerElement = document.querySelector("#search-container");
             let currentDisplay = searchContainerElement.style.display;
-            if (!currentDisplay || currentDisplay == 'none'){
+            if (!currentDisplay || currentDisplay == 'none') {
                 searchContainerElement.style.display = "block";
             } else {
-                searchContainerElement.style.display = "none";  }
-            });
-        
-        document.querySelector("#btnSearch").addEventListener("click", async function(){
+                searchContainerElement.style.display = "none";
+            }
+        });
+
+        document.querySelector("#btnSearch").addEventListener("click", async function () {
             let clearResult = document.querySelector("#results");
             clearResult.innerHTML = "";
             searchResultLayer.clearLayers();
-            
+
             let searchTerms = document.querySelector("#searchTerms").value;
             let boundaries = map.getBounds();
             let center = boundaries.getCenter();
@@ -27,28 +29,28 @@ window.addEventListener("DOMContentLoaded", async function(){
             let searchResults = await search(latlng, searchTerms, categoryID);
 
             let searchResultElement = document.querySelector("#results");
-            for (let r of searchResults.results){
+            for (let r of searchResults.results) {
                 console.log(r);
                 //Display the marker
                 let lat = r.geocodes.main.latitude;
                 let lng = r.geocodes.main.longitude;
 
                 let marker = L.marker([lat, lng]).addTo(searchResultLayer);
-                marker.bindPopup(function(){
+                marker.bindPopup(function () {
                     let el = document.createElement('div');
                     el.classList.add("popup");
                     el.innerHTML = `<h1>${r.name}</h1>`
 
-                async function getPicture(){
-                    let photos = await getPhoto(r.fsq_id);
-                    let firstPhoto = photos[0];
-                    let url = firstPhoto.prefix + "100x100" + firstPhoto.suffix;
-                    el.innerHTML += `<img src="${url}"/>`
-                }
+                    async function getPicture() {
+                        let photos = await getPhoto(r.fsq_id);
+                        let firstPhoto = photos[0];
+                        let url = firstPhoto.prefix + "100x100" + firstPhoto.suffix;
+                        el.innerHTML += `<img src="${url}"/>`
+                    }
                     getPicture();
                     return el;
                 });
-                
+
                 let resultElement = document.createElement("div");
                 resultElement.innerText = r.name;
                 resultElement.classList.add("search-result");
@@ -62,35 +64,3 @@ window.addEventListener("DOMContentLoaded", async function(){
     }
     init();
 });
-
-function initMap() { 
-    // create a map object and set center point zoom
-    let map = L.map('map').setView([1.3521, 103.8198], 11);
-
-    // need set up the tile layer
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
-    }).addTo(map);
-    console.log(Response.data);
-    return map; // return map as a result of the function
-}
-
-//create a marker cluster
-let markerClusterLayer = L.markerClusterGroup();
-let markerClusterLayerList = [];
-
-function getRandomLatLng(map) {
-    let bounds = map.getBounds();
-    let southWest = bounds.getSouthWest();
-    let northEast = bounds.getNorthEast();
-    let lngSpan = northEast.lng - southWest.lng;
-    let latSpan = northEast.lat - southWest.lat;
-    let randomLng = Math.random() * lngSpan + southWest.lng;
-    let randomLat = Math.random() * latSpan + southWest.lat;
-    return [ randomLat, randomLng,];
-    }
