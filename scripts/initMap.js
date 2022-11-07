40d14f82ce5fe950bc1f78a72dc9bf1f1ad89bde
unknown <sudinosg@protonmail.com>
2022-11-07 21:25:28 +0800

    change map size

--- scripts/initMap.js
+++ scripts/initMap.js
@@ -1,5 +1,5 @@
 function initMap() {
-    let map = L.map('map').setView([1.3521, 103.8198], 12);
+    let map = L.map('map').setView([1.3521, 103.8198], 11);
 
     // tile layer
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
